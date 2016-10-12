caeaea63e4bea39785a5f42455757d57a0f6d93c
Saurabh Hindlekar <saurabhhindlekar@hotmail.com>
2016-10-12 18:37:21 -0400

    print fibonacci from start to end pyscript, concept of yield

--- fibo_recursive/pyscript.py
+++ fibo_recursive/pyscript.py
@@ -1,5 +1,20 @@
-def F(n):
-  if n ==0: return 0
-  elif n ==1: return 1
-  else: return F(n-1) + F(n-2)
-print F(5)
+#def F(n):
+#  if n ==0: return 0
+#  elif n ==1: return 1
+#  else: return F(n-1) + F(n-2)
+def F():
+  a,b=0,1
+  yield a
+  yield b
+  while True:
+    a,b = b, b+a
+    yield b
+
+def subFib(startNumber, endNumber):
+  for cur in F():
+    if cur > endNumber: return
+    if cur >= startNumber:
+      yield cur
+for i in subFib(10,200):
+  print i
+#print F(5)
