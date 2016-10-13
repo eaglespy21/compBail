fc1be47e936bf46a4f5c3193ae6d6cf4750425ea
Saurabh Hindlekar <saurabhhindlekar@hotmail.com>
2016-10-12 20:00:46 -0400

    Fibonacci Analysis complete

--- fibo_recursive/pyscript.py
+++ fibo_recursive/pyscript.py
@@ -1,7 +1,9 @@
-#def F(n):
-#  if n ==0: return 0
-#  elif n ==1: return 1
-#  else: return F(n-1) + F(n-2)
+#storeFib = {0:0,1:1}
+def F(n):
+  if n ==0: return 0
+  elif n ==1: return 1
+  else: 
+    return F(n-1) + F(n-2)
 def F():
   a,b=0,1
   yield a
@@ -9,12 +11,23 @@ def F():
   while True:
     a,b = b, b+a
     yield b
+def mem_fib(n, _cache={}):
+  if n in _cache:
+    return _cache[n]
+  elif n > 1:
+    return _cache.setdefault(n, mem_fib(n-1) + mem_fib(n-2))
+  return n #He is Manuel Nueuer for 0,1 
 
 def subFib(startNumber, endNumber):
   for cur in F():
     if cur > endNumber: return
     if cur >= startNumber:
       yield cur
-for i in subFib(10,200):
+for i in subFib(0,1000000000):
   print i
-#print F(5)
+#for i in range(40):
+#  storeFib[i] = F(i)
+#  print storeFib[i]
+#  print F(i)
+#  print mem_fib(i)
+
